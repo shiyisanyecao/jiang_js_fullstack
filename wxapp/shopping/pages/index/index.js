5e83f0c6cd945f43e16064dc6ba060680b02f208
shiyisanyecao <2327532812@qq.com>
2018-05-24 23:02:26 +0800

    购物车的学习

--- wxapp/shopping/pages/index/index.js
+++ wxapp/shopping/pages/index/index.js
@@ -1,54 +1,75 @@
 //index.js
 //获取应用实例
+import goods from '../../api/goods.js' 
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    num:1,
+    show:false,
+    totalNum:0,
+    scaleCart:false,
+    currentTab:0,
+    goods: null
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  onLoad(options) {
+    const id = options.id || 2;
+    let curGoods;
+    for(let i = 0 ; i < goods.length; i++) {
+      if(goods[i].id === id) {
+        curGoods = goods[i];
+        break;
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+    }
+    setTimeout(()=>{
+      this.setData({
+        goods:curGoods
+      });
+    },1000)
+    this.setData({
+      goods:curGoods
+    });
+  },
+  switchTab(e) {
+    var currentTab = this.data.currentTab;
+    console.log(e)
+    if(currentTab != e.target.dataset.index) {
+      this.setData({
+        currentTab: e.target.dataset.index
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  bindChange(e) {
+    var currentTab = this.data.currentTab;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      currentTab:e.target.dataset.index
     })
+  },
+  addCount() {
+    let num = this.data.num;
+    num ++;
+    this.setData({
+      num
+    })
+  },
+  addToCart() {
+    const num = this.data.num;
+    const total = this.data.totalNum;
+    this.setData({
+      show:true
+    });
+    setTimeout(()=>{
+      this.setData({
+        show:false,
+        scaleCart:true
+      });
+      setTimeout(()=>{
+        this.setData({
+          scaleCart:false,
+          hasCarts:true,
+          totalNum:num + total
+        })
+      },200)
+    },300)
   }
 })

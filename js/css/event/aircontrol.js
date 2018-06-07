cacc741b9eca84af550b3b538d51e37603204dd1
shiyisanyecao <2327532812@qq.com>
2018-06-07 15:42:22 +0800

    BFC

---
+++ js/css/event/aircontrol.js
@@ -0,0 +1,22 @@
+const TV = {
+    open:function(){
+        console.log('打开电视');
+    },
+    close:function(){
+        console.log('关闭电视');
+    }
+}
+const AirConditional = {
+    // 统一的接口
+    open:function(){
+        console.log('打开空调');
+    },
+    close:function(){
+        console.log('关闭空调');
+    }
+}
+function setCommand(obj) {
+    const o = {};
+    o.execute = obj.open();
+    o.execute = obj.close();
+}
---
+++ js/css/event/readme.md
@@ -0,0 +1,20 @@
+1 PC
+px markman
+
+2 文档流 栅格系统
+网格比例
+bootstrap
+12列 col-md-？
+
+3 链接 详情页 点图片
+a.href /detail target="blank"
+
+地图图片 
+map.baidu.com
+
+4 内容 格式比较工整
+左边标题 右边内容
+table
+类名 绿色文字 g_color 类名
+按钮 btn bootstrap primary
+vertical-align middle

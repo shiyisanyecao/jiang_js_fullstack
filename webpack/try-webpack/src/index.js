fef97615228a837aa5d9841b086ca70c91f06051
shiyisanyecao <2327532812@qq.com>
2018-07-13 17:40:52 +0800

    add to repos

--- webpack/try-webpack/src/index.js
+++ webpack/try-webpack/src/index.js
@@ -1,4 +1,8 @@
 // 文件引用 按照顺序打包文件 文件依赖
-require('./style.less')
+require('./styles/normalize')
+require('./styles/index')
+const format = require('utils/format')
 const { log } = require('./utils')
-log('hello world')
+log(format('hello world'))
+document.querySelector('.superman').style.display = 'block'
+log(_.map([1,2,3],(item) => item * 2))
--- webpack/try-webpack/webpack.config.js
+++ webpack/try-webpack/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 console.log(path.resolve(__dirname, 'dist'))
 module.exports = {
     entry: './src/index.js',
@@ -17,6 +20,15 @@ module.exports = {
                 use: 'babel-loader'
             },
             {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader'
+                    ]
+                })
+            },
+            {
                 test: /\.less$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
@@ -25,10 +37,49 @@ module.exports = {
                         'less-loader'
                     ]
                 })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
             }
         ]
     },
+    resolve: {
+        alias: {
+            utils: path.resolve(__dirname, 'src/utils')
+        },
+        extensions: ['.js', '.json', '.css', '.less']
+    },
     plugins: [
-        new ExtractTextPlugin('[name].css')
-    ]
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin({
+            file: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets/favicon.ico',
+                to: 'favicon.ico'
+            }
+        ]),
+        // lodash 作为工具 是很多组件会使用的 省去了到处import
+        new webpack.ProvidePlugin({
+            '_': 'lodash'
+        })
+    ],
+    devServer: {
+        port: '1314',
+        before(app) {
+            app.get('/api/test.json', (req, res) => {
+                res.json({
+                    code: 200,
+                    message: 'hello world'
+                })
+            })
+        }
+    }
 }
